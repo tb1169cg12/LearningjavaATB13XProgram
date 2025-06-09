8cc4af7750b0e75c43d8d34ab5504837a68e94c2
apple <tb1169cg12@gmail.com>
2025-06-09 10:10:02 +0530

    new

--- src/ex_01_Java_Basic/Lab007_keywords_and_identifiers.java
+++ src/ex_01_Java_Basic/Lab007_keywords_and_identifiers.java
@@ -3,8 +3,8 @@ package ex_01_Java_Basic;
 public class Lab007_keywords_and_identifiers {
     public static void main(String[] args) {
         System.out.println("how many keywords are there and how many identifiers ");
-        System.out.println("answer- 5 keywords and 1 identifier ");
+        System.out.println("answer- 5 keywords and 3 identifier ");
         System.out.println("keywords - package,public,class,static,void ");
-        System.out.println("Identifier-Lab007_keywords_and_identifiers");
+        System.out.println("Identifier-Lab007_keywords_and_identifiers,ex_01_java_basics,args");
     }
 }
