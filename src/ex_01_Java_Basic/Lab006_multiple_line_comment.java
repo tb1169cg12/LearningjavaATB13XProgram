4e7b1200af956154aca8ea9b465c4ccf3a024923
apple <tb1169cg12@gmail.com>
2025-06-09 09:45:34 +0530

    new

---
+++ src/ex_01_Java_Basic/Lab006_multiple_line_comment.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Lab006_multiple_line_comment {
+    public static void main(String[] args) {
+
+      /*  this program is
+                used to demonstrate multiple line comment */
+        System.out.println("this is multiple line comment");
+    }
+}
