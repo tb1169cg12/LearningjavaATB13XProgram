d3f4025a4a9243fc8e9da6384b4419636e4f3aa0
apple <tb1169cg12@gmail.com>
2025-06-09 09:42:35 +0530

    new

---
+++ src/ex_01_Java_Basic/Lab001_9june.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Lab001_9june {
+    public static void main(String[] args) {
+//       this is a  comment , this code will not be executed
+     /* System.out.println("hi");
+        System.out.println("bye");
+        System.out.println("drink");*/
+        System.out.println("chai");
+    }
+
+}
---
+++ src/ex_01_Java_Basic/Lab005_single_line_comment.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab005_single_line_comment {
+    public static void main(String[] args) {
+        System.out.println("hi");
+//        this program is to demonatrate single line command
+    }
+}
