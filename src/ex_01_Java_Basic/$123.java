7acb0bca0b7e22f2e0b053ec9cdc3374bf24f294
apple <tb1169cg12@gmail.com>
2025-06-10 10:51:12 +0530

    new

---
+++ src/ex_01_Java_Basic/$123.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class $123 {
+    public static void main(String[] args) {
+        System.out.println("program to check name of java can start with special character $");
+    }
+}
