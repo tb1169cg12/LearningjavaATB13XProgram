23aebd7420a90209d75dd83b84fb9758380b778d
apple <tb1169cg12@gmail.com>
2025-06-10 10:55:04 +0530

    new

---
+++ src/ex_01_Java_Basic/this_is_a_very_long_class_name_hi_how_are_you.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class this_is_a_very_long_class_name_hi_how_are_you {
+    public static void main(String[] args) {
+        System.out.println("program to check how long name can be given to program");
+    }
+}
