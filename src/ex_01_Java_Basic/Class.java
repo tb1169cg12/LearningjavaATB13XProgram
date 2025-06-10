5b06d930ec8ce4edf16e5f5915df17a66ca4f575
apple <tb1169cg12@gmail.com>
2025-06-10 10:53:35 +0530

    new

---
+++ src/ex_01_Java_Basic/Class.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Class {
+    public static void main(String[] args) {
+        System.out.println("program to see if we can make java file with name Class but we cant with class");
+    }
+}
