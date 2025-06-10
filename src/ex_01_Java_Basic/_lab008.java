c97fd51f9cfc2fa7c6a02f46dd548c6f5487f61c
apple <tb1169cg12@gmail.com>
2025-06-10 10:56:00 +0530

    new

---
+++ src/ex_01_Java_Basic/_lab008.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class _lab008 {
+    public static void main(String[] args) {
+        System.out.println("program to check name can start with special character _");
+    }
+}
