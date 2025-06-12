af90d22cee9bb4c6846f807743664873bcf0742f
apple <tb1169cg12@gmail.com>
2025-06-12 11:02:52 +0530

    new

---
+++ src/ex_02_Java_Basics/Lab_10JUNE_009.java
@@ -0,0 +1,18 @@
+package ex_02_Java_Basics;
+
+public class Lab_10JUNE_009 {
+    public static void main(String[] args) {
+        byte b= 127;
+        byte c= -127;
+        int i =10;
+        short s=11;
+        long l=12343232432l;
+        long L=1232144234223L;
+        //byte d=-129;
+      //  byte c =128;
+        //byte b wont print as its >127
+        System.out.println(L);
+
+
+    }
+}
