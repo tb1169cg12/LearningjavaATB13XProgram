e68ec893f38d1a9c42e1b4c04a8622bbf4a0a62a
apple <tb1169cg12@gmail.com>
2025-06-12 10:43:40 +0530

    new

---
+++ src/ex_02_Java_Basics/Lab_10JUNE_008.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics;
+
+public class Lab_10JUNE_008 {
+    public static void main(String[] args) {
+        boolean is_rahu_married= true;
+        boolean is_rahul_having_audi= false;
+        System.out.println(is_rahul_having_audi);
+
+    }
+}
