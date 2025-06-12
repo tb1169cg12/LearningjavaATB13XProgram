f6c77ced014c3c0dd69a3eea8e1e036d95f2b786
apple <tb1169cg12@gmail.com>
2025-06-12 10:01:00 +0530

    new

---
+++ src/ex_02_Java_Basics/Lab_008.java
@@ -0,0 +1,18 @@
+package ex_02_Java_Basics;
+
+public class Lab_008 {
+    public static void main(String[] args) {
+        //data_type variable_name= variable_value
+        byte b= 10;
+        //byte-data type-type of container
+        //store only interer value -limited to max and min
+        //-128 to 127
+
+        //b-variable name-identifier
+
+        //assignmentassign value to b
+
+        // 10-literal-integer value
+
+    }
+}
