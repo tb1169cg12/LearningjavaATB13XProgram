a7c5b0c640402572c96372ca207589635f4ae659
apple <tb1169cg12@gmail.com>
2025-06-12 15:13:25 +0530

    9 table

---
+++ src/ex_02_Java_Basics/Lab_10JUNE_014.java
@@ -0,0 +1,18 @@
+package ex_02_Java_Basics;
+
+public class Lab_10JUNE_014 {
+    public static void main(String[] args) {
+       /*changing value of a
+        int a=10;
+        a=20;
+        System.out.println(a);*/
+        //changing value of a by using final command
+        final int a=20;
+        //a=30;   this will not be allowed will show system error
+        System.out.println(a);
+
+        /*float PI=3.414;
+        System.out.printF("PI");
+        */
+    }
+}
