c40e2b71d7c088aa5441ffe362ae7abc7c75b249
apple <tb1169cg12@gmail.com>
2025-06-12 10:04:38 +0530

    new

---
+++ src/ex_02_Java_Basics/Lab_009.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basics;
+
+public class Lab_009 {
+    public static void main(String[] args) {
+        int a= 10;
+        a = 65 + 1;
+        System.out.println(a);
+        //a was varible so we can change it in 2nd line
+
+
+    }
+}
