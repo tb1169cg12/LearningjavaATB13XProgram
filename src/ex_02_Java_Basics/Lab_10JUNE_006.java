e7282bece87ce8e4fdfe20edc31e94730c83b3b1
apple <tb1169cg12@gmail.com>
2025-06-12 10:36:51 +0530

    new

--- src/ex_02_Java_Basics/Lab_10JUNE_006.java
+++ src/ex_02_Java_Basics/Lab_10JUNE_006.java
@@ -3,8 +3,10 @@ package ex_02_Java_Basics;
 public class Lab_10JUNE_006 {
     public static void main(String[] args) {
 
-        int enum=10;
+       /* int enum=10;
         System.out.println(enum);
+
+        */
         //not allowed as enum is a keyword
         int Enum = 10;
         System.out.println(Enum);
---
+++ src/ex_02_Java_Basics/Lab_10JUNE_007.java
@@ -0,0 +1,18 @@
+package ex_02_Java_Basics;
+
+public class Lab_10JUNE_007 {
+    public static void main(String[] args) {
+        int a_b =11;
+       /* int rahul bajaj =11;
+        System.out.println(rahul bajaj);
+       space not allowed*/
+        /*
+        int enum =10;
+        System.out.println(enum);
+        enum not allowed as its a keyword*/
+        int a$b =10;
+        System.out.println(a$b);
+        //allowed
+
+    }
+}
