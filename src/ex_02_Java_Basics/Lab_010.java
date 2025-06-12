1b2e657417a3ede99b85aa0d97796090d187e225
apple <tb1169cg12@gmail.com>
2025-06-12 10:08:43 +0530

    new

---
+++ src/ex_02_Java_Basics/Lab_010.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basics;
+
+public class Lab_010 {
+    public static void main(String[] args) {
+        int age = 76;
+        age = age+1;
+        age = 76;
+        // reassigned value again to variable
+        System.out.println(age);
+
+    }
+}
