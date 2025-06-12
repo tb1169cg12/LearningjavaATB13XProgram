e0a349d55e6108856f798767ed8fff09dd10a39b
apple <tb1169cg12@gmail.com>
2025-06-12 22:59:48 +0530

    block

---
+++ src/ex_02_Java_Basics/Lab_10JUNE_016.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics;
+
+public class Lab_10JUNE_016 {
+    public static void main(String[] args) {
+        //int local;
+       // System.out.println(local);
+        //this will throw error -variable local might not have been initialized
+        int local=1;
+        System.out.println(local);
+    }
+}
---
+++ src/ex_03_literrals/Lab_10JUNE_001.java
@@ -0,0 +1,23 @@
+package ex_03_literrals;
+
+public class Lab_10JUNE_001 {
+    public static void main(String[] args) {
+
+
+        char a = 'A';
+        System.out.println(a);
+        System.out.println("rahulbajaj");
+
+        char new_line = '\n';//wil insert in new line
+        char tab_line = '\t';//will insert 1 tab space
+        char back_space = '\b';//will deleat 1 word
+        char carriage_return ='\r';//will dealear whole word from front
+        System.out.println("rahul"+new_line+"bajaj");
+        System.out.println("ahul"+tab_line+"bajaj");
+        System.out.println("rahul"+back_space+"bajaj");
+        System.out.println("rahul"+carriage_return+"bajaj");
+
+
+
+    }
+}
