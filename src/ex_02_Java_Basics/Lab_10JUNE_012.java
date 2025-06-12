451ab65ab85b7d0fde3dd4c6456537869a265079
apple <tb1169cg12@gmail.com>
2025-06-12 14:48:24 +0530

    new

---
+++ src/ex_02_Java_Basics/Lab_10JUNE_012.java
@@ -0,0 +1,17 @@
+package ex_02_Java_Basics;
+
+public class Lab_10JUNE_012 {
+    public static void main(String[] args) {
+        int a =10;
+        System.out.printf("your variable name is %d",a);
+        System.out.println("------");
+        /*program to preint line with variable
+        %d →> int, byte, long, short, - data type
+        %s > string,
+        %f > float,double
+        %b >bollean
+         */
+        int b=20;
+        System.out.printf("%d + %d",a,b);
+    }
+}
