2b352c575e741bbf023171c69025fd2e7bb8b101
apple <tb1169cg12@gmail.com>
2025-06-12 15:02:56 +0530

    9 table

---
+++ src/ex_02_Java_Basics/Lab_10JUNE_013.java
@@ -0,0 +1,28 @@
+package ex_02_Java_Basics;
+
+public class Lab_10JUNE_013 {
+    public static void main(String[] args) {
+        int a =9;
+        int b=1;
+        System.out.printf("%d x %d=%d",a,b,a*b);
+        System.out.println("");
+        System.out.printf("%d x %d=%d",a,b+1,a*(b+1));
+        System.out.println("");
+        System.out.printf("%d x %d=%d",a,b+2,a*(b+2));
+        System.out.println("");
+        System.out.printf("%d x %d=%d",a,b+3,a*(b+3));
+        System.out.println("");
+        System.out.printf("%d x %d=%d",a,b+4,a*(b+4));
+        System.out.println("");
+        System.out.printf("%d x %d=%d",a,b+5,a*(b+5));
+        System.out.println("");
+        System.out.printf("%d x %d=%d",a,b+6,a*(b+6));
+        System.out.println("");
+        System.out.printf("%d x %d=%d",a,b+7,a*(b+7));
+        System.out.println("");
+        System.out.printf("%d x %d=%d",a,b+8,a*(b+8));
+        System.out.println("");
+        System.out.printf("%d x %d=%d",a,b+9,a*(b+9));
+
+    }
+}
