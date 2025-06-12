f7e3e52279369fad5508c8f5aa912481fc4b6e22
apple <tb1169cg12@gmail.com>
2025-06-12 14:39:44 +0530

    new

---
+++ src/ex_02_Java_Basics/Lab_10JUNE_010.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics;
+
+public class Lab_10JUNE_010 {
+    public static void main(String[] args) {
+        byte b=10;
+        System.out.println(b);
+        //byte b = 10; → 10 → 00001010 (binary) → this is store in the memory - 8 Bits = 1 Byte
+
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab_10JUNE_011.java
@@ -0,0 +1,14 @@
+package ex_02_Java_Basics;
+
+public class Lab_10JUNE_011 {
+    public static void main(String[] args) {
+        int a =1;
+        //single line
+        System.out.print(a);
+        System.out.print(a);
+        //multiple line ln means new line
+        int b =2;
+        System.out.println(b);
+        System.out.println(b);
+    }
+}
