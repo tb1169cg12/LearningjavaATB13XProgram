19d85d25c78f973c35da0ed719a0c7f89894b379
apple <tb1169cg12@gmail.com>
2025-06-12 10:27:07 +0530

    new

--- src/ex_02_Java_Basics/Lab_10JUNE_006.java
+++ src/ex_02_Java_Basics/Lab_10JUNE_006.java
@@ -2,8 +2,11 @@ package ex_02_Java_Basics;
 
 public class Lab_10JUNE_006 {
     public static void main(String[] args) {
+
         int enum=10;
         System.out.println(enum);
         //not allowed as enum is a keyword
+        int Enum = 10;
+        System.out.println(Enum);
     }
 }
