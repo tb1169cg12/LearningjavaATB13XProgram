6e1bcc4852ce7ef5cbd9c2d67af0e9fd2bb769cb
apple <tb1169cg12@gmail.com>
2025-06-14 23:42:13 +0530

    urinary operator

---
+++ src/ex_03_literrals/Lab_11JUNE_relational_operator.java
@@ -0,0 +1,21 @@
+package ex_03_literrals;
+
+public class Lab_11JUNE_relational_operator {
+    public static void main(String[] args) {
+        //relational operators
+        //< less than
+        //<= less than equal to
+        // > greater than
+        // >= greater than equal to
+        // == equal to
+        //!= not equal to
+       int a =10;
+       int b =20;
+       boolean c=a>b;
+       boolean d=a<b;
+
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
