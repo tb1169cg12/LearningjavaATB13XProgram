65cc63569459ab87ad9feaaac53059c17dfd40dd
apple <tb1169cg12@gmail.com>
2025-06-14 22:02:10 +0530

    block

---
+++ src/ex_03_literrals/Lab_11JUNE_Operators.java
@@ -0,0 +1,26 @@
+package ex_03_literrals;
+
+public class Lab_11JUNE_Operators {
+    public static void main(String[] args) {
+        int age =65;
+        //int-data type
+        //age-identifier
+        //= assignment operator
+        //65 literal
+        //arthematic operator
+        //+ addition
+        //- substraction
+        // * multiplication
+        // /division
+        // % modulus-returns remainder
+        int a =12;
+        int b =2;
+        float c= 3.0f;
+        System.out.printf("a+b= %d ",a+b).println();
+        System.out.printf("a*b=%d",a*b).println();
+        System.out.printf("a-b=%d",a-b).println();
+        System.out.println(a/c);
+        System.out.println(a%b);
+
+    }
+}
