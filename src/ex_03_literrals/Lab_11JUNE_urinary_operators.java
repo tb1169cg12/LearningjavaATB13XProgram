8d69a7d7cb4a15380075ac5cec2c517613e399b9
apple <tb1169cg12@gmail.com>
2025-06-14 22:14:37 +0530

    urinary operator

---
+++ src/ex_03_literrals/Lab_11JUNE_urinary_operators.java
@@ -0,0 +1,17 @@
+package ex_03_literrals;
+
+public class Lab_11JUNE_urinary_operators {
+    public static void main(String[] args) {
+        //unary operators
+        int a= +1;
+        int b= -100;
+        int result=a+b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(result);
+
+        int i=-1;
+        i=i+1;
+        System.out.println(i);
+    }
+}
