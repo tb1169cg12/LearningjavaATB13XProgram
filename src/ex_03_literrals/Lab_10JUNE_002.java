726f5879daed734a4f256867762838df460c8851
apple <tb1169cg12@gmail.com>
2025-06-14 20:51:07 +0530

    block

---
+++ src/ex_03_literrals/Lab_10JUNE_002.java
@@ -0,0 +1,17 @@
+package ex_03_literrals;
+
+public class Lab_10JUNE_002 {
+    public static void main(String[] args) {
+        //primitive data type -defined by java guys
+        //cannot be broken further
+        // max,min size defined
+        byte b=10;//1 byte=8bits
+        int age=65;//4 byte=32 bits
+        //non primitve data type -defined by user
+        //no max min size
+        //8 byte 64 bits
+        String name="rahul";//string is bunch of characters
+        String s=null;
+
+    }
+}
---
+++ src/ex_03_literrals/Lab_11JUNE_003.java
@@ -0,0 +1,15 @@
+package ex_03_literrals;
+
+public class Lab_11JUNE_003 {
+    public static void main(String[] args) {
+        int i=9;
+        int j=1;
+        System.out.printf("%d x %d = %d",i,j,9).println();
+        System.out.printf("%d x %d = %d",i,j+1,i*(j+1)).println();
+//.println(); will print new lie
+
+        System.out.println(i+j);
+        System.out.println(i*j);
+        System.out.println(i-j);
+    }
+}
