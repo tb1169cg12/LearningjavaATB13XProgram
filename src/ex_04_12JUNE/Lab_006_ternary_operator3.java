e0d5d7a525f6a39c7b4127377d1cc6ce159a9197
apple <tb1169cg12@gmail.com>
2025-06-15 18:57:12 +0530

    urinary operator

---
+++ src/ex_04_12JUNE/Lab_006_ternary_operator3.java
@@ -0,0 +1,15 @@
+package ex_04_12JUNE;
+
+public class Lab_006_ternary_operator3 {
+    public static void main(String[] args) {
+        // The maximum number between two numbers by using ternary operator.
+        int a=1;
+        int b=2;
+       // String which_is_max= a>b ? "a is max" : "b is max";
+      //  System.out.println(which_is_max);
+        //or we can use pre defined function
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.min(a,b));
+    }
+
+}
