564e59f2747bbc803201acca96130a0d0adfb7ce
apple <tb1169cg12@gmail.com>
2025-06-15 19:05:21 +0530

    urinary operator

---
+++ src/ex_04_12JUNE/Lab_007_ternary_operator4.java
@@ -0,0 +1,9 @@
+package ex_04_12JUNE;
+
+public class Lab_007_ternary_operator4 {
+    public static void main(String[] args) {
+        int a=10;
+        String result =(a%2==0)?"even":"odd";
+        System.out.printf(result);
+    }
+}
