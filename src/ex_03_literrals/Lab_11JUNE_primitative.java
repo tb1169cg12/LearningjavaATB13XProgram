b466a25ec4d77418079e9d9ea029c32340e2901e
apple <tb1169cg12@gmail.com>
2025-06-15 11:16:12 +0530

    urinary operator

---
+++ src/ex_03_literrals/Lab_11JUNE_primitative.java
@@ -0,0 +1,12 @@
+package ex_03_literrals;
+
+public class Lab_11JUNE_primitative {
+    public static void main(String[] args) {
+        System.out.println(10==10);
+        System.out.println(10<10);
+        System.out.println(10>20);
+        System.out.println(10<20);
+        System.out.println(!(10<20));
+        System.out.println(true || true);
+    }
+}
