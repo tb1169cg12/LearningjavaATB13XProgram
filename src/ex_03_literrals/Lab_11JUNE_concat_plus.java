d18d66b49b3c7f68b52a1608fd86ce83b149f02b
apple <tb1169cg12@gmail.com>
2025-06-15 10:51:13 +0530

    urinary operator

---
+++ src/ex_03_literrals/Lab_11JUNE_concat_plus.java
@@ -0,0 +1,14 @@
+package ex_03_literrals;
+
+public class Lab_11JUNE_concat_plus {
+    public static void main(String[] args) {
+        String first_name ="rahul";
+        String last_name ="bajaj";
+        int a=10;
+        int b=20;
+
+        System.out.println(first_name + last_name+a+b);//if string comes first it will act as string
+        System.out.println(a+b+first_name+last_name);// if int comes first it act like int
+        System.out.println(first_name+last_name+(a+b));//bodmas principle
+    }
+}
