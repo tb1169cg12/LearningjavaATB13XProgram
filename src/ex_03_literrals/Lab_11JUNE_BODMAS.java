88933f5e68ebc584e91ed5a53dd384b64517819b
apple <tb1169cg12@gmail.com>
2025-06-15 11:11:46 +0530

    urinary operator

---
+++ src/ex_03_literrals/Lab_11JUNE_BODMAS.java
@@ -0,0 +1,11 @@
+package ex_03_literrals;
+
+public class Lab_11JUNE_BODMAS {
+    public static void main(String[] args) {
+        System.out.println((9*3/9+1)*3);
+        //9 * 3 - 27
+        // 27/9 -> 3
+        // 3+1 -> 4
+        // 4 * 3 -> 12
+    }
+}
