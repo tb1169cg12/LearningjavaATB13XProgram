4e2782b24612e52b6ecad73d693e5388aff58449
apple <tb1169cg12@gmail.com>
2025-06-15 12:48:35 +0530

    urinary operator

---
+++ src/ex_03_literrals/Lab_11JUNE_concat_new.java
@@ -0,0 +1,11 @@
+package ex_03_literrals;
+
+public class Lab_11JUNE_concat_new {
+    public static void main(String[] args) {
+        int a=10;
+        char c='A';
+        System.out.println(a+c);
+        System.out.println(c);
+        System.out.println(c+c);
+    }
+}
