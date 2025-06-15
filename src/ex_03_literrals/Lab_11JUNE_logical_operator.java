30c597839d72c7df792a09c410108f5d93067a4c
apple <tb1169cg12@gmail.com>
2025-06-15 10:30:47 +0530

    urinary operator

---
+++ src/ex_03_literrals/Lab_11JUNE_logical_operator.java
@@ -0,0 +1,14 @@
+package ex_03_literrals;
+
+public class Lab_11JUNE_logical_operator {
+    public static void main(String[] args) {
+        boolean a= true;
+        boolean c= true|| false;
+        Boolean d= true && false;
+        System.out.println(!a);
+
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
