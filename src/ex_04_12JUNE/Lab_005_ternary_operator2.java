782392d3a0d87cdf6ccae22d0efb03a70f5686cf
apple <tb1169cg12@gmail.com>
2025-06-15 16:19:49 +0530

    urinary operator

---
+++ src/ex_04_12JUNE/Lab_005_ternary_operator2.java
@@ -0,0 +1,10 @@
+package ex_04_12JUNE;
+
+public class Lab_005_ternary_operator2 {
+    public static void main(String[] args) {
+        int a=-10;
+        //positive negative checker
+        String is_number_positive_or_negative = a>0 ?"positive" : "negative";
+        System.out.println(is_number_positive_or_negative);
+    }
+}
