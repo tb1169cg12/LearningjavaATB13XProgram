ef3f0b7e9c9285a1c4d00d7bad026379cfec31ce
apple <tb1169cg12@gmail.com>
2025-06-15 16:15:53 +0530

    urinary operator

---
+++ src/ex_04_12JUNE/Lab_004_ternary_operator.java
@@ -0,0 +1,11 @@
+package ex_04_12JUNE;
+
+public class Lab_004_ternary_operator {
+    public static void main(String[] args) {
+        //ternary operator evaluate a condition and return one of two values based on the result.
+        //age to vote-18
+        int age =10;
+        String canivote = age>18 ? "yes" : "no";
+        System.out.println(canivote);
+    }
+}
