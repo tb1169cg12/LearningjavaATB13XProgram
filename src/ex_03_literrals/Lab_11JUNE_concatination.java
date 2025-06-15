9f08fb8ec0c67dd1bf48236f95cbf5aed332c44e
apple <tb1169cg12@gmail.com>
2025-06-15 10:40:15 +0530

    urinary operator

---
+++ src/ex_03_literrals/Lab_11JUNE_concatination.java
@@ -0,0 +1,15 @@
+package ex_03_literrals;
+
+public class Lab_11JUNE_concatination {
+    public static void main(String[] args) {
+        //concatination -adding or joining
+        System.out.println("rahul" + "bajaj");
+        //System.out.println("rahul" -"bajaj"); not supported concatination type
+        //System.out.println("rahul" / "bajaj"); not supported
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+        //+ operator behaves differently with different data type
+
+    }
+}
