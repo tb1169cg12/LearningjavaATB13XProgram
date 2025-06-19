c266a7a5d624e97a52206e84b564d8fa2620ae7a
apple <tb1169cg12@gmail.com>
2025-06-19 10:04:21 +0530

    exam

---
+++ src/exam19june/question2.java
@@ -0,0 +1,13 @@
+package exam19june;
+
+public class question2 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        System.out.printf("addition:%d",a+b).println();
+        System.out.printf("subtraction:%d",a-b).println();
+        System.out.printf("multiplication:%d",a*b).println();
+        System.out.printf("divison:%d",a/b).println();
+        System.out.printf("modulus:%d",a%b).println();
+    }
+}
