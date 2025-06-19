00b1ac784ac1dfee3d0b2d472eaab151439f76ce
apple <tb1169cg12@gmail.com>
2025-06-19 10:22:45 +0530

    exam

---
+++ src/exam19june/question4.java
@@ -0,0 +1,15 @@
+package exam19june;
+
+public class question4 {
+    public static void main(String[] args) {
+        int a=5;
+
+        System.out.printf("original:%d",a).println();
+        System.out.printf("Pre-increment:%d",++a).println();
+        System.out.printf("Post-increment:%d",a++).println();
+        System.out.printf("Pre-decrement:%d",a--).println();
+        System.out.printf("Post-decrement:%d",--a).println();
+
+
+    }
+}
