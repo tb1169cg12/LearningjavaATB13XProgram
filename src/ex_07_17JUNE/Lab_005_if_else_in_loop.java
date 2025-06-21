28389f6d2f29b5ef6686707a15466b94700525ea
apple <tb1169cg12@gmail.com>
2025-06-21 16:46:58 +0530

    exam

---
+++ src/ex_07_17JUNE/Lab_005_if_else_in_loop.java
@@ -0,0 +1,13 @@
+package ex_07_17JUNE;
+
+public class Lab_005_if_else_in_loop {
+    public static void main(String[] args) {
+        for(int age=0;age<20;age++){
+            if(age<16){
+            System.out.println("no gifts");}
+            else{
+                System.out.println("iphone");
+            }
+        }
+    }
+}
