7edb468df77cb315e42bee313c6ccd66e292a13f
apple <tb1169cg12@gmail.com>
2025-06-21 15:18:33 +0530

    exam

---
+++ src/ex_06_16JUNE/Lab_004_exercise.java
@@ -0,0 +1,23 @@
+package ex_06_16JUNE;
+
+import com.sun.source.tree.DefaultCaseLabelTree;
+
+public class Lab_004_exercise {
+    public static void main(String[] args) {
+       // int a=10;
+       // switch(a){
+       //     case
+      //  }
+        char ch='A';
+        switch(ch) {
+            case 65:
+                System.out.println("character is ASCII");
+                break;
+            default:
+                System.out.println("no match");
+        }
+
+
+
+    }
+}
---
+++ src/ex_06_16JUNE/Lab_005_duplicate_Case.java
@@ -0,0 +1,14 @@
+package ex_06_16JUNE;
+
+public class Lab_005_duplicate_Case {
+    public static void main(String[] args) {
+        int a=98;
+        switch(a){
+            case 98:
+                System.out.println("allowed");
+            //case 98:
+               // System.out.println("not allowed");// duplicate not allowed
+        }
+
+    }
+}
