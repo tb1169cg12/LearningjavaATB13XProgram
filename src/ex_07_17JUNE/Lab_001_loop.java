84a507a7882e20f2ff0574a87ae574c6b450c660
apple <tb1169cg12@gmail.com>
2025-06-21 16:12:45 +0530

    exam

---
+++ src/ex_07_17JUNE/Lab_001_loop.java
@@ -0,0 +1,14 @@
+package ex_07_17JUNE;
+
+public class Lab_001_loop {
+    public static void main(String[] args) {
+        // For Loop
+        // Help you to repeat a block of code.
+        // Initialization -> Condition -> Updation (Incre/decre)
+        //
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+        }
+
+    }
+}
