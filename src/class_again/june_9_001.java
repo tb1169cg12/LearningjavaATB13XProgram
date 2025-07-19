4c1f864f8b1168ad6a4462e2379a7250eda54f3f
apple <tb1169cg12@gmail.com>
2025-07-19 16:33:04 +0530

    exam

---
+++ src/class_again/june_9_001.java
@@ -0,0 +1,15 @@
+package class_again;
+
+public class june_9_001 {
+/**
+ * Author - Pramod
+ * Course : ATB13*
+ * This is a documentation comment
+ * @param args command-line arguments
+ * @return void
+ */
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/exam19june/newprogram.java
@@ -0,0 +1,9 @@
+package exam19june;
+
+public class newprogram {
+    public static void main(String[] args) {
+    int a=10;
+    int b=20;
+        System.out.printf("%d+%d=%d",a,b,a+b);
+    }
+}
