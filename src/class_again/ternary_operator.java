36a0a6e85f4a1fa823be01ce272f13ce8b5d73b5
apple <tb1169cg12@gmail.com>
2025-07-20 15:24:19 +0530

    exam

---
+++ src/class_again/ternary_operator.java
@@ -0,0 +1,9 @@
+package class_again;
+
+public class ternary_operator {
+    public static void main(String[] args) {
+        int a=9;
+        String b = a<10 ? "a is small" : "a is big";
+        System.out.println(b);
+    }
+}
