3ae8655bce500bda4a12659f33c07ca319b614e3
apple <tb1169cg12@gmail.com>
2025-07-26 13:35:28 +0530

    exam

--- src/ex_10_20JUNE/FUNCTION.java
+++ src/ex_10_20JUNE/FUNCTION.java
@@ -3,8 +3,12 @@ package ex_10_20JUNE;
 public class FUNCTION {
     public static void main(String[] args) {
     f1();//call function
+        name_of_the_function(); //call again 2nd one also
     }
     static void f1 (){
         System.out.println("hi");
+    }
+    static void name_of_the_function (){
+        System.out.println("hey");
     }
 }
