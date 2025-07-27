ee4e6b42c2e3e923bd44553ab95958b1e5aafea8
apple <tb1169cg12@gmail.com>
2025-07-27 20:09:14 +0530

    exam

---
+++ src/ex_10_20JUNE/aray.java
@@ -0,0 +1,10 @@
+package ex_10_20JUNE;
+
+public class aray {
+    public static void main(String[] args) {
+        int [] marks = {1,2,3,4,5};
+        System.out.println(marks.length);
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+    }
+}
