a9d9d62804ad16841cbe11609fd0c9f056d796da
apple <tb1169cg12@gmail.com>
2025-07-27 09:34:59 +0530

    exam

---
+++ src/ex_10_20JUNE/FUNCTION2.java
@@ -0,0 +1,12 @@
+package ex_10_20JUNE;
+
+public class FUNCTION2 {
+
+        static void my_function(){
+            System.out.println("hi");
+        }
+        static int mu_funtion(){
+            return 10;
+        }
+    }
+
---
+++ src/ex_10_20JUNE/string.java
@@ -0,0 +1,9 @@
+package ex_10_20JUNE;
+
+public class string {
+    public static void main(String[] args) {
+        String name ='sonal';
+        System.out.println(name.length());
+
+    }
+}
