d0a67e28a3650bffa256cd940eb1deb0202e022b
apple <tb1169cg12@gmail.com>
2025-07-27 15:28:30 +0530

    exam

--- src/ex_10_20JUNE/string.java
+++ src/ex_10_20JUNE/string.java
@@ -6,5 +6,9 @@ public class string {
         System.out.println(name.length());
         System.out.println(name.charAt(3));
         System.out.println(name.concat( "patel"));
+        System.out.println(name.contains("sonal"));
+        System.out.println(name.equals("sonal"));
+        System.out.println(name.length());
+        System.out.println(name.replace("a","A"));
     }
 }
---
+++ src/ex_10_20JUNE/types_of_string.java
@@ -0,0 +1,15 @@
+package ex_10_20JUNE;
+
+public class types_of_string {
+    public static void main(String[] args) {
+        String s0 ="pramod";
+        String s1 = new String("Pramod");
+        StringBuffer stringbuffer =new StringBuffer("sheela");
+        StringBuilder stringBuilder = new StringBuilder("muni");
+
+        System.out.println(s0);
+        System.out.println(s1);
+        System.out.println(stringbuffer);
+        System.out.println(stringBuilder);
+    }
+}
