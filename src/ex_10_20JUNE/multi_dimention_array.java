c8ba1f51642346ee433ed738944975d2c387923b
apple <tb1169cg12@gmail.com>
2025-08-03 17:05:09 +0530

    exam

--- src/ex_10_20JUNE/multi_dimention_array.java
+++ src/ex_10_20JUNE/multi_dimention_array.java
@@ -11,7 +11,7 @@ public class multi_dimention_array {
         for (int i=0;i< matrix.length;i++)
         {
         for (int j=0;j<matrix[i].length;j++){
-        System.out.print(matrix[i][j]);
+        System.out.print(matrix[i][j]+" | ");
         }
         System.out.println();
         }
