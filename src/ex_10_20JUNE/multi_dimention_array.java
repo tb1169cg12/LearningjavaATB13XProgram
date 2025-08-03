67f0aa5a0280ed2a75dff4a7f29474d9fac7bafe
apple <tb1169cg12@gmail.com>
2025-08-03 16:53:26 +0530

    exam

--- src/ex_10_20JUNE/multi_dimention_array.java
+++ src/ex_10_20JUNE/multi_dimention_array.java
@@ -7,12 +7,23 @@ public class multi_dimention_array {
                 {4,5,6},
                 {7,8,9}
         };
+        System.out.println(matrix.length);
+        for (int i=0;i< matrix.length;i++)
+        {
+        for (int j=0;j<matrix[i].length;j++){
+        System.out.print(matrix[i][j]);
+        }
+        System.out.println();
+        }
+
         int[][] martrix2;
         martrix2= new int[][]{
                 {10,20},
                 {20,30},
                 {30,40}
         };
+
+
         int [][] array =new int[3][3];
         array[0][0]=1;
         array[0][1]=2;
@@ -25,6 +36,13 @@ public class multi_dimention_array {
         array[2][0]=7;
         array[2][1]=8;
         array[2][2]=9;
+
+        //jagged array
+        int[][] jagged_array={
+                {1,2},{1,3},
+                {2,2},{2,4},{3,4},
+                {2,6}
+        };
 
     }
 }
