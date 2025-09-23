9c0aeebaa79aa8b6f30d8e1fad54b888b5cb3b7c
apple <tb1169cg12@gmail.com>
2025-09-23 22:42:34 +0530

    exam

---
+++ src/a1/a2.java
@@ -0,0 +1,10 @@
+package a1;
+
+public class a2 {
+    public static void main(String[] args) {
+    f1();
+    }
+    static void f1(){
+        System.out.println("hi");
+    }
+}
---
+++ src/a1/function.java
@@ -0,0 +1,11 @@
+package a1;
+
+public class function {
+    public static void main(String[] args) {
+    f1();//function call
+    }
+    //step 1 function dfn
+    static void f1(){
+        System.out.println("hi");
+    }
+}
