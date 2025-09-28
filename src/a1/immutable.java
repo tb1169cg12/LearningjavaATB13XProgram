0485cd6f7ebf3c0cfd9a6dd914420bd746e2d6b9
apple <tb1169cg12@gmail.com>
2025-09-28 23:03:04 +0530

    exam

--- src/a1/immutable.java
+++ src/a1/immutable.java
@@ -2,15 +2,17 @@ package a1;
 
 public class immutable {
     public static void main(String[] args) {
-        String a="hello";
+        String a="hello";//present in string constant pool
         String b="hello";
         String c="hello";
         System.out.println(a);
 
-        String s1=new String("hello");
+        String s1=new String("hello");//present in object area
         String s2=new String("hello");
         String s3=new String("hello");
         System.out.println(s1);
         //total 3 string only in string pool a,s1,s2,s3
+        System.out.println(a==b);//code to find if bot stings are in same area
+        System.out.println(a==s1);//will return true if in same and false if in different pool
     }
 }
