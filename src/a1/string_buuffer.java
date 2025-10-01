07a6d5cc7b1a3a4d3fc45858d110ac3924e4f4f1
apple <tb1169cg12@gmail.com>
2025-10-01 10:31:38 +0530

    exam

---
+++ src/a1/string_buuffer.java
@@ -0,0 +1,18 @@
+package a1;
+
+public class string_buuffer {
+    public static void main(String[] args) {
+        String a="pramod";
+        String b=new String("pramod");
+        StringBuffer c=new StringBuffer("pramod");
+        c.append("dutta");
+        System.out.println(c);//conctiate
+        c.reverse();//reverse
+        System.out.println(c);
+
+
+
+        StringBuilder d=new StringBuilder("pramod");
+
+    }
+}
