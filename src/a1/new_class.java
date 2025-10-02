b94f796e380b7135685d2217cab98470c204440c
apple <tb1169cg12@gmail.com>
2025-10-02 09:21:03 +0530

    exam

---
+++ src/a1/new_class.java
@@ -0,0 +1,9 @@
+package a1;
+
+import java.util.Scanner;
+
+public class new_class {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+    }
+}
---
+++ src/a1/wover_consonent.java
@@ -0,0 +1,10 @@
+package a1;
+
+public class wover_consonent {
+    public static void main(String[] args) {
+        //Java Program to Count Vowels and Consonants
+        String input ="Java Program to Count Vowels and Consonants";
+        input=input.toLowerCase();
+        System.out.println(input);
+    }
+}
