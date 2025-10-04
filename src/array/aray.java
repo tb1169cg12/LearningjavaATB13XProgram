58e3067d471ffc5d169a83a0a269f3de719dd975
apple <tb1169cg12@gmail.com>
2025-10-04 16:37:00 +0530

    exam

---
+++ src/array/aray.java
@@ -0,0 +1,26 @@
+package array;
+
+public class aray {
+    public static void main(String[] args) {
+        //pre defined arrays
+        int[] marks={1,2,3,4};
+        System.out.println(marks[0]);
+        System.out.println(marks.length);
+        int[] mat=new int[3];
+        boolean[] bolean={true,false};
+        System.out.println(bolean[0]);
+        char c='C';
+        String d="rahul ";
+        System.out.println(c);
+        //crate aray
+        int[] mark=new int[5];//aray of size 5//declaring aray
+        String[] name= new String[1];//declaring
+        mark[0]='2';
+        mark[1]='2';
+        mark[2]='2';
+        mark[3]='2';
+        mark[4]='2';
+        System.out.println(mark[0]);
+
+    }
+}
---
+++ src/array/array2.java
@@ -0,0 +1,11 @@
+package array;
+
+public class array2 {
+    public static void main(String[] args) {
+        String[] names={"rahul","tarun","sonu","monu"};
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+
+        }
+
+}}
