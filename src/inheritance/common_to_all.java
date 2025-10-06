eef15727fa33b649f3bd7522fa23bb5c660ff37c
apple <tb1169cg12@gmail.com>
2025-10-06 10:20:36 +0530

    exam

---
+++ src/inheritance/common_to_all.java
@@ -0,0 +1,20 @@
+package inheritance;
+
+public class common_to_all {
+    void starting_a_browser(){
+        System.out.println("start  the browser");
+    }
+    void closing_the_browser(){
+        System.out.println("closing the browser");
+
+    }
+    void read_excell_file(){
+        System.out.println("read_excell_file");
+
+    }
+    void read_database_file(){
+        System.out.println("read database file");
+    }
+
+}
+
