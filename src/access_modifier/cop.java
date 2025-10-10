53b6b6714942f1749282b02efccf437bd60d98cd
apple <tb1169cg12@gmail.com>
2025-10-10 10:21:26 +0530

    exam

---
+++ src/access_modifier/cop.java
@@ -0,0 +1,13 @@
+package access_modifier;
+
+public class cop {
+    public int gun;
+    String icard;
+    public cop(int bullet){
+        this.gun=bullet;
+
+    }
+    public void canishoot(){
+        System.out.println("yes u can");
+    }
+}
---
+++ src/access_modifier/criminal/thief.java
@@ -0,0 +1,11 @@
+package access_modifier.criminal;
+
+import access_modifier.cop;
+
+public class thief {
+    public static void main(String[] args) {
+        cop thief = new cop(100);
+        thief.gun=100;
+        thief.canishoot();
+    }
+}
