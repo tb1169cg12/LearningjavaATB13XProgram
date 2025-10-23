0028870822aeef007928200a154368bf98210f32
apple <tb1169cg12@gmail.com>
2025-10-23 09:44:02 +0530

    exam

--- src/static_exercise/Static.java
+++ src/static_exercise/Static.java
@@ -7,9 +7,17 @@ public class Static {
         System.out.println(P.a);
         p1.a=20;
         System.out.println(P.a);
+        p1.b=30;
+        System.out.println(p1.b);
     }
 }
 
 class P{
     static int a=10;
+    int b=10;
+    static {
+        //this is static block
+        //this is called when class is loaded , see line 5 when P is mentioned
+        System.out.println(" this is called when class is loaded");
+    }
 }
