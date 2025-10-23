e2908d91d2948532a49e6a549d5539a5aea93268
apple <tb1169cg12@gmail.com>
2025-10-23 09:54:56 +0530

    exam

---
+++ src/static_exercise/iib.java
@@ -0,0 +1,19 @@
+package static_exercise;
+
+public class iib {
+    public static void main(String[] args) {
+        A a=new A();
+    }
+}
+class A{
+    A(){
+        System.out.println("dc");
+    }
+    {
+        System.out.println("ji i am iib");
+
+    }
+    {
+        System.out.println("if u want to call or execute something");
+    }
+}
